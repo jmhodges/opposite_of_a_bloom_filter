10ddbbf0ef85d08c87a730d07c7cdd30302f1550
Jeff Hodges <jeff@somethingsimilar.com>
2012-05-20 01:05:58 -0700

    initial import of go implementation

---
+++ go/oppobloom/oppobloom.go
@@ -0,0 +1,80 @@
+package oppobloom
+
+import (
+	"bytes"
+	"crypto/md5"
+	"errors"
+	"hash"
+	"math"
+	"sync/atomic"
+	"unsafe"
+)
+
+type Filter struct {
+	array []*[]byte
+	sizeMask uint32
+}
+
+var ErrSizeTooLarge = errors.New("oppobloom: size given to large to round to a power of 2")
+var ErrSizeTooSmall = errors.New("oppobloom: filter cannot have a zero or negative size")
+var MaxFilterSize = 1 << 30
+
+func NewFilter(size int) (*Filter, error) {
+	if size > MaxFilterSize {
+		return nil, ErrSizeTooLarge
+	}
+	if size <= 0 {
+		return nil, ErrSizeTooSmall
+	}
+	// round to the next largest power of two
+	size = int(math.Pow(2, math.Ceil(math.Log2(float64(size)))))
+	slice := make([]*[]byte, size)
+	sizeMask := uint32(size - 1)
+	return &Filter{slice, sizeMask}, nil
+}
+
+func (f *Filter) Contains(id []byte) bool {
+	h := md5UintHash{md5.New()}
+	h.Write(id)
+	uindex := h.Sum32() & f.sizeMask
+	index := int32(uindex)
+	oldId := getAndSet(f.array, index, id)
+	return bytes.Equal(oldId, id)
+}
+
+func (f *Filter) Size() int {
+	return len(f.array)
+}
+
+type md5UintHash struct {
+	hash.Hash // a hack with knowledge of how md5 works
+}
+
+func (m md5UintHash) Sum32() uint32 {
+	sum := m.Sum(nil)
+	x := uint32(sum[0])
+	for _, val := range sum[1:4] {
+		x = x << 3
+		x += uint32(val)
+	}
+	return x
+}
+
+// Returns the id that was in the slice at the given index after putting the
+// new id in the slice at that index, atomically.
+func getAndSet(arr []*[]byte, index int32, id []byte) []byte {
+	indexPtr := (*unsafe.Pointer)(unsafe.Pointer(&arr[index]))
+	idUnsafe := unsafe.Pointer(&id)
+	var oldId []byte
+	for {
+		oldIdUnsafe := atomic.LoadPointer(indexPtr)
+		if atomic.CompareAndSwapPointer(indexPtr, oldIdUnsafe, idUnsafe) {
+			oldIdPtr := (*[]byte)(oldIdUnsafe)
+			if oldIdPtr != nil {
+				oldId = *oldIdPtr
+			}
+			break
+		}
+	}
+	return oldId
+}
