2f7e1f36b289b7722910aff4d90158638df8afda
Jeff Hodges <jeff@somethingsimilar.com>
2012-05-19 17:41:54 -0700

    initial import of java implementation

---
+++ java/src/main/java/com/somethingsimilar/opposite_of_a_bloom_filter/ByteArrayFilter.java
@@ -0,0 +1,73 @@
+package com.somethingsimilar.opposite_of_a_bloom_filter;
+
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import com.google.common.math.IntMath;
+
+/**
+ * ByteArrayFilter is used to filter out duplicate byte arrays from a given dataset or stream. It is
+ * guaranteed to never return a false positive (that is, it will never say that an item has already
+ * been seen by the filter when it has not) but may return a false negative.
+ *
+ * ByteArrayFilter is thread-safe.
+ */
+public class ByteArrayFilter {
+  private static final HashFunction HASH_FUNC = Hashing.murmur3_32();
+  private final int size;
+  private final AtomicReferenceArray<byte[]> array;
+  private static final int MAX_SIZE = 1 << 30;
+
+  /**
+   * Constructs a ByteArrayFilter with an underlying array of the given size, rounded up to the next
+   * power of two.
+   *
+   * This rounding occurs because the hashing is much faster on an array the size of a power of two.
+   * If you really want a different sized array, used the AtomicReferenceArray constructor.
+   *
+   * @param size The size of the underlying array.
+   */
+  public ByteArrayFilter(int size) {
+    if (size <= 0) {
+      throw new IllegalArgumentException("array size must be greater than zero, was " + size);
+    }
+    if (size > MAX_SIZE) {
+      throw new IllegalArgumentException(
+          "array size may not be larger than 2**31-1, but will be rounded to larger. was " + size);
+    }
+    // round to the next largest power of two
+    this.size = IntMath.pow(2, IntMath.log2(size, RoundingMode.CEILING));
+    this.array = new AtomicReferenceArray<byte[]>(size);
+  }
+
+  /**
+   * Returns whether the given byte array has been previously seen by this array. That is, if a byte
+   * array with the same bytes as id has been passed to to this method before.
+   *
+   * This method may return false when it has seen an id before. This occurs if the id passed in
+   * hashes to the same index in the underlying array as another id previously checked. On the
+   * flip side, this method will never return true incorrectly.
+   *
+   * @param id The byte array that may have been previously seen.
+   * @return Whether the byte array is contained in the ByteArrayFilter.
+   */
+  public boolean contains(byte[] id) {
+    HashCode code = HASH_FUNC.hashBytes(id);
+    int index = Math.abs(code.asInt()) % size;
+    byte[] oldId = array.getAndSet(index, id);
+    return Arrays.equals(id, oldId);
+  }
+
+  /**
+   * Returns the size of the underlying array. Welp.
+   *
+   * @return The size of the underlying array.
+   */
+  public int getSize() {
+    return size;
+  }
+}
---
+++ java/src/test/java/com/somethingsimilar/opposite_of_a_bloom_filter/ByteArrayFilterTest.java
@@ -0,0 +1,70 @@
+package com.somethingsimilar.opposite_of_a_bloom_filter;
+
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class ByteArrayFilterTest {
+
+  @Test
+  public void testTheBasics() {
+    ByteArrayFilter filter = new ByteArrayFilter(2);
+    byte[] twentyNineId = new byte[]{27, 28, 29};
+    byte[] thirtyId = new byte[]{27, 28, 30};
+    byte[] thirtyThreeId = new byte[]{27, 28, 33};
+    assertFalse("nothing should be contained at all", filter.contains(twentyNineId));
+    assertTrue("now it should", filter.contains(twentyNineId));
+    assertFalse("false unless the hash collides", filter.contains(thirtyId));
+    assertTrue("original should still return true", filter.contains(twentyNineId));
+    assertTrue("new array should still return true", filter.contains(thirtyId));
+
+    // Handling collisions. {27, 28, 33} and {27, 28, 30} hash to the same index using the current
+    // hash function inside ByteArrayFilter.
+    assertFalse("colliding array returns false", filter.contains(thirtyThreeId));
+    assertTrue(
+        "colliding array returns true in second call", filter.contains(thirtyThreeId));
+    assertFalse("original colliding array returns false", filter.contains(thirtyId));
+    assertTrue("original colliding array returns true", filter.contains(thirtyId));
+    assertFalse("colliding array returns false", filter.contains(thirtyThreeId));
+  }
+
+  @Test
+  public void testSizeRounding() {
+    ByteArrayFilter filter = new ByteArrayFilter(3);
+    assertEquals("3 should round to 4", 4, filter.getSize());
+    filter = new ByteArrayFilter(4);
+    assertEquals("4 should round to 4", 4, filter.getSize());
+    filter = new ByteArrayFilter(129);
+    assertEquals("129 should round to 256", 256, filter.getSize());
+  }
+
+  @Test
+  public void testTooLargeSize() {
+    int size = (1<<30) + 1;
+    try {
+      new ByteArrayFilter(size);
+      fail("should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException e) {
+      String msg =
+          "array size may not be larger than 2**31-1, but will be rounded to larger. was " + size;
+      assertEquals(msg, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testTooSmallSize() {
+    int size = 0;
+    try {
+      new ByteArrayFilter(size);
+      fail("zero passed in directly should have thrown an IllegalArgumentException");
+    } catch (IllegalArgumentException e) {
+      String msg = "array size must be greater than zero, was 0";
+      assertEquals("zero passed in directly should error correctly", msg, e.getMessage());
+    }
+  }
+}
