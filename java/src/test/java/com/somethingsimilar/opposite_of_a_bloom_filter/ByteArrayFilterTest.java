e546377a94d77edd3460088122cdf0647983fbdb
Jeff Hodges <jeff@somethingsimilar.com>
2012-05-20 23:48:41 -0700

    add LICENSE

--- java/src/test/java/com/somethingsimilar/opposite_of_a_bloom_filter/ByteArrayFilterTest.java
+++ java/src/test/java/com/somethingsimilar/opposite_of_a_bloom_filter/ByteArrayFilterTest.java
@@ -1,3 +1,6 @@
+// Copyright 2012 Jeff Hodges. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 package com.somethingsimilar.opposite_of_a_bloom_filter;
 
 import org.junit.Test;
